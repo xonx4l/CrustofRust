397f123f795e07ae68ded5bab88dfebaf63cb337
xonx <119700621+xonx4l@users.noreply.github.com>
2024-09-01 18:16:59 +0530

    Create iterators.rs

---
+++ iterators.rs
@@ -0,0 +1,20 @@
+pub struct Flatten<O> {
+    outer: O
+}
+
+impl<O> Flatten<O> {
+    fn new(iter:O) -> Self {
+        Flatten { outer: iter }
+    }
+}
+
+impl<O> Iterator for Flatten<O> 
+where 
+     O: Iterator,
+     O::Item: IntoIterator,
+{
+    type Item = <O::Item as IntoIterator>::Item;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.outer.next().and_then(|inner| inner.into_iter().next())
+    }
+}
